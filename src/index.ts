a38b12cff1c9b51c9add09241f38fe2801b28636
name <yumapc96@gmail.com>
2023-03-20 12:30:32 +0900

    add:友達追加時の処理を追加した

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,39 @@ app.get("/webhook", (req: express.Request, res: express.Response) => {
     res.sendStatus(200);
 });
 
+app.get("/webhook", (req: express.Request, res: express.Response) => {
+    Promise.all(req.body.events.map(handleEvent)).then((result) => {
+        res.json(result);
+    });
+});
+
 const client = new line.Client(config);
+
+// webhookイベントの処理
+const handleEvent = async (event: any): Promise<any> => {
+    switch (event.type) {
+        case "follow":
+            return await followEvent(event);
+        case "unfollow":
+            // エラー回避
+            return;
+        case "message":
+            return;
+        default:
+            return;
+    }
+};
+
+const followEvent = async (event: any) => {
+    try {
+        await client.replyMessage(event.replyToken, {
+            type: "text",
+            text: "Thank you following!!",
+        });
+    } catch (err) {
+        console.log(err);
+    }
+};
 
 app.listen(port, () => {
     console.log(`Server running on ${port}`);
